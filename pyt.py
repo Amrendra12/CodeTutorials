55f50999f0e5d20fcae3704a5874d1d29fb4b864
Amrendra Singh <amrendra2k6@gmail.com>
2019-01-27 01:10:34 +0530

    adding new file

---
+++ pyt.py
@@ -0,0 +1,18 @@
+import pytest
+
+@pytest.fixture()
+def my_fixture1():
+    print "\nI'm the 1stfixture"
+
+
+@pytest.fixture()
+def my_fixture2():
+    print "\nI'm the 2ndfixture"
+	
+@pytest.mark.usefixtures('my_fixture1')
+class Test:
+    def test1(self):
+        print "I'm the test 1"
+
+    def test2(self):
+        print "I'm the test 2"
